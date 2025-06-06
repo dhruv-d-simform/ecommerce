bb42e438da6b58b790128ca1576003d5a068d634
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-06 15:27:20 +0530

    feature: Implement the Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router';
+import { Header } from '@/components/Header';
 
 function App() {
     return (
         <div>
+            <Header />
             <div className="max-w-[100rem] mx-auto pt-16">
                 <Outlet />
             </div>
