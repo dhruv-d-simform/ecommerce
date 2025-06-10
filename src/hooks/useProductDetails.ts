7bc6a59bc716ed1d90824c4e8ca11ddf4d603d8a
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-10 10:59:36 +0530

    refractor: Move functions to fetch data in utils folder

--- src/hooks/useProductDetails.ts
+++ src/hooks/useProductDetails.ts
@@ -1,18 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
+import { fetchProductDetails } from '@/utils/fetchData';
 import type { Product } from '@/types/product.types';
 
 export function useProductDetails(productId: number | string) {
     return useQuery<Product, Error>({
         queryKey: ['product', productId],
-        queryFn: async () => {
-            const res = await fetch(
-                `https://dummyjson.com/products/${productId}`
-            );
-            if (!res.ok) {
-                throw new Error('Failed to fetch product');
-            }
-            return res.json();
-        },
+        queryFn: () => fetchProductDetails(productId),
         enabled: !!productId,
     });
 }
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,21 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
+import { fetchProducts } from '@/utils/fetchData';
 import type { ProductResponse } from '@/types/product.types';
 
 export function useProducts() {
     return useQuery<ProductResponse, Error>({
         queryKey: ['products'],
-        queryFn: async () => {
-            const res = await fetch(
-                'https://dummyjson.com/products?skip=25&limit=100'
-            );
-
-            if (!res.ok) {
-                console.error('API error:', res.status, res.statusText);
-                throw new Error('Error occured while fetching the Products.');
-            }
-
-            const data = await res.json();
-            return data;
-        },
+        queryFn: fetchProducts,
     });
 }
