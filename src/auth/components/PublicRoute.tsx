1e454f10c9553caff0cf1723d8a541908e2f4c36
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-10 17:18:43 +0530

    fix: Update the Public Route to handle update Session Context

--- src/auth/components/PublicRoute.tsx
+++ src/auth/components/PublicRoute.tsx
@@ -1,8 +1,20 @@
 import { Navigate } from 'react-router';
 import { useSession } from '@/auth/contexts/SessionContext';
+import { Spinner } from '@/components/ui/spinner';
 
 export function PublicRoute({ children }: React.PropsWithChildren) {
-    const session = useSession();
+    const { session, loading } = useSession();
+
+    if (loading) {
+        return (
+            <div className="flex flex-col items-center justify-center min-h-[40vh] py-16">
+                <div className="mb-8 relative">
+                    <Spinner />
+                </div>
+            </div>
+        );
+    }
+
     if (session) {
         return <Navigate to="/" replace />;
     }
